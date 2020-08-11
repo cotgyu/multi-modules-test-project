691c5e5d655efd5065f1285b3391bd44aff00a74
cotgyu <v123v123s@naver.com>
2020-08-12 08:17:46 +0900

    feature 모듈생성, 서브 모듈 설정

---
+++ module-web/src/test/java/com/example/multimodulestestproject/ModuleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multimodulestestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
