ddcf9ca29e7d20fdbbda6926f252e567813dca6d
cotgyu <v123v123s@naver.com>
2020-08-12 08:14:30 +0900

    Initial commit

---
+++ src/main/java/com/example/multimodulestestproject/MultiModulesTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimodulestestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModulesTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModulesTestProjectApplication.class, args);
+	}
+
+}
